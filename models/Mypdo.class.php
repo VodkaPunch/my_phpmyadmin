7c81036218327394d1112b92a34856d285b094a6
VodkaPunch <boris.maigrot@neuf.fr>
2017-12-22 09:47:43 +0100

    please quoi

--- models/Mypdo.class.php
+++ models/Mypdo.class.php
@@ -15,7 +15,7 @@ class Mypdo extends PDO
 			$bool=false;
 		}
 		try {
-			$this->dbo =parent::__construct("mysql:dbname=".DBNAME.";host=".DBHOST.";", DBUSER, DBPASSWD,
+			$this->dbo =parent::__construct("mysql:host=".DBHOST.";", DBUSER, DBPASSWD,
 			array(PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => $bool, PDO::ERRMODE_EXCEPTION => $bool));
 		}
 		catch (PDOException $e) {
